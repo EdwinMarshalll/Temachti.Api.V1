d1524be7b9d58dbd65c1858b49e872d0bda824ed
Edwin Marshall <edwin.lozanoe@outlook.com>
2023-04-21 18:41:50 -0600

    Api version added to HATEOAS

--- Utils/LinksGenerator.cs
+++ Utils/LinksGenerator.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Temachti.Api.DTOs;
@@ -11,36 +12,13 @@ public class LinksGenerator
     private readonly IAuthorizationService authorizationService;
     private readonly IHttpContextAccessor httpContextAccessor;
     private readonly IActionContextAccessor actionContextAccessor;
-
+    
     public LinksGenerator(IAuthorizationService authorizationService, IHttpContextAccessor httpContextAccessor, IActionContextAccessor actionContextAccessor)
     {
         this.authorizationService = authorizationService;
         this.httpContextAccessor = httpContextAccessor;
         this.actionContextAccessor = actionContextAccessor;
     }
-
-    private IUrlHelper BuildURLHelper()
-    {
-        var factory = httpContextAccessor.HttpContext.RequestServices.GetRequiredService<IUrlHelperFactory>();
-        return factory.GetUrlHelper(actionContextAccessor.ActionContext);
-    }
-
-    #region Links
-    public async Task GenerateLinks(DTOTechnology dtoTechnology)
-    {
-        var isAdmin = await IsAdmin();
-        var Url = BuildURLHelper();
-
-        dtoTechnology.Links.Add(new HATEOASData(Url.Link("getTechnologyById", new { id = dtoTechnology.Id }), rel: "self", method: "GET"));
-        dtoTechnology.Links.Add(new HATEOASData(Url.Link("getTechnologyByCode", new {code = dtoTechnology.Code}), rel: "self", method: "GET"));
-
-        if(isAdmin)
-        {
-            dtoTechnology.Links.Add(new HATEOASData(Url.Link("updateTechnology", new { id = dtoTechnology.Id }), rel: "update technology", method: "PUT"));
-            dtoTechnology.Links.Add(new HATEOASData(Url.Link("deleteTechnology", new { id = dtoTechnology.Id }), rel: "delete technology", method: "DELETE"));
-        }
-    }
-    #endregion
 
     #region ROLES
     public async Task<bool> IsAdmin()
@@ -50,4 +28,41 @@ public class LinksGenerator
         return result.Succeeded;
     }
     #endregion
+
+    private IUrlHelper BuildURLHelper()
+    {
+        var factory = httpContextAccessor.HttpContext.RequestServices.GetRequiredService<IUrlHelperFactory>();
+        return factory.GetUrlHelper(actionContextAccessor.ActionContext);
+    }
+
+    private string GetVersion()
+    {
+        var headers = httpContextAccessor.HttpContext.Request.Headers;
+        if(headers.ContainsKey("x-version"))
+        {
+            return headers["x-version"];
+        }
+
+        return "1";
+    }
+
+    #region Links
+    public async Task GenerateLinks(DTOTechnology dtoTechnology)
+    {
+        var isAdmin = await IsAdmin();
+        var Url = BuildURLHelper();
+        var version = GetVersion();
+
+        dtoTechnology.Links.Add(new HATEOASData(Url.Link($"getTechnologyByIdV{version}", new { id = dtoTechnology.Id }), rel: "self", method: "GET"));
+        dtoTechnology.Links.Add(new HATEOASData(Url.Link($"getTechnologyByCodeV{version}", new {code = dtoTechnology.Code}), rel: "self", method: "GET"));
+
+        if(isAdmin)
+        {
+            dtoTechnology.Links.Add(new HATEOASData(Url.Link($"updateTechnologyV{version}", new { id = dtoTechnology.Id }), rel: "update technology", method: "PUT"));
+            dtoTechnology.Links.Add(new HATEOASData(Url.Link($"deleteTechnologyV{version}", new { id = dtoTechnology.Id }), rel: "delete technology", method: "DELETE"));
+        }
+    }
+    #endregion
+
+    
 }
